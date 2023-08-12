1358cdb9dd77e7a6ae5f80c8d22bcfe657f9b5b2
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 13:19:10 +0500

    feat: добавил все инструменты

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -5,6 +5,13 @@ const Toolbar = () => {
     return (
         <div className="toolbar">
             <button className="toolbar__btn brush"/>
+            <button className="toolbar__btn rect"/>
+            <button className="toolbar__btn circle"/>
+            <button className="toolbar__btn eraser"/>
+            <button className="toolbar__btn line"/>
+            <button className="toolbar__btn undo"/>
+            <button className="toolbar__btn redo"/>
+            <button className="toolbar__btn save"/>
         </div>
     );
 };
