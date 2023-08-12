d161fd4a99237a14068176d59b365563ef431a12
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 18:21:56 +0500

    feat: подключил Bootstrap

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -4,6 +4,7 @@ import {observer} from "mobx-react-lite";
 import canvasState from "../store/canvasState";
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import {Button, Modal} from "react-bootstrap";
 
 const Canvas = observer(() => {
     const canvasRef = useRef()
@@ -19,6 +20,20 @@ const Canvas = observer(() => {
 
     return (
         <div className="canvas">
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <canvas onMouseDown={() => mouseDownHandler()} ref={canvasRef} width={600} height={400}></canvas>
         </div>
     );
