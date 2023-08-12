589404fa49a42d01a8877e55299dc30b57bf617c
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 19:20:14 +0500

    feat: установил axios и сделал заготовку для отправки рисунка с сервера

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const WSServer = require('express-ws')(app)
 const aWss = WSServer.getWss()
+const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
+
+app.use(cors())
+app.use(express.json())
 
 const PORT = process.env.PORT || 5001
 
@@ -19,7 +25,6 @@ app.ws('/', (ws, req) => {
         }
     })
 })
-app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
 
 const connectionHandler = (ws, msg) => {
     ws.id = msg.id
@@ -33,3 +38,23 @@ const broadcastConnection = (ws, msg) => {
         }
     })
 }
+
+app.post('/image', (req, res) => {
+    try {
+        const data = req.body.img.replace(`data:image/png;base64,`, '')
+        fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('error')
+    }
+})
+app.get('/image', (req, res) => {
+    try {
+
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json('error')
+    }
+})
+
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
